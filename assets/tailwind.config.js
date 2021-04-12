68153a2b769e0ecf7412805dbc4db884ab4f6802
c4710n <c4710n@users.noreply.github.com>
2021-04-13 00:44:49 +0800

    chore: replace `assets`

---
+++ assets/tailwind.config.js
@@ -0,0 +1,43 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  purge: {
+    content: [
+      '../lib/**/*.html.eex',
+      '../lib/**/*.html.leex',
+      '../lib/**/*.ex',
+      '../lib/**/*.exs',
+      './js/**/*.js',
+    ],
+  },
+  theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      white: colors.white,
+      black: colors.black,
+
+      primary: colors.rose,
+      neutral: colors.blueGray,
+      info: colors.blue,
+      success: colors.green,
+      danger: colors.red,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
