cae21c0f254d1f9c2d80688c485e70832b144e03
c4710n <c4710n@users.noreply.github.com>
2021-04-13 12:38:30 +0800

    feat: add ElcWeb.Endpoint.subscribe/0

--- lib/elc_web/presence.ex
+++ lib/elc_web/presence.ex
@@ -3,15 +3,24 @@ defmodule ElcWeb.Presence do
     otp_app: :elc,
     pubsub_server: Elc.PubSub
 
+  alias ElcWeb.Endpoint
+
   @user_presence_topic "presence_state"
 
+  @doc false
   def track_user(pid, uid) do
     __MODULE__.track(pid, @user_presence_topic, uid, %{
       online_at: System.system_time(:second)
     })
   end
 
+  @doc false
   def list_users() do
     __MODULE__.list(@user_presence_topic)
+  end
+
+  @doc false
+  def subscribe() do
+    Endpoint.subscribe(@user_presence_topic)
   end
 end
