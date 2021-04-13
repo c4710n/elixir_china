18e029eb39e78ec103be898d15ed95fe3bc6b3dc
c4710n <c4710n@users.noreply.github.com>
2021-04-13 12:37:51 +0800

    feat: add support for browser_id

--- lib/elc_web/live/mount_helpers.ex
+++ lib/elc_web/live/mount_helpers.ex
@@ -1,12 +1,17 @@
 defmodule ElcWeb.MountHelpers do
   import Phoenix.LiveView, only: [assign: 3]
 
-  def assign_defaults(socket) do
+  def assign_defaults(socket, session) do
     socket
     |> assign_session_id(session)
+    |> assign_browser_id(session)
   end
 
   defp assign_session_id(socket, session) do
     assign(socket, :session_id, session["live_socket_id"])
+  end
+
+  defp assign_browser_id(socket, session) do
+    assign(socket, :browser_id, session["_csrf_token"])
   end
 end
