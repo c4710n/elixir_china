63e6fcf9c18f1d8bece992368cc6804ff8e8ed30
c4710n <c4710n@users.noreply.github.com>
2022-12-25 16:18:39 +0800

    feat: custom error pages

--- lib/elixir_china_web/controllers/error_html.ex
+++ lib/elixir_china_web/controllers/error_html.ex
@@ -5,15 +5,16 @@ defmodule ElixirChinaWeb.ErrorHTML do
   # uncomment the embed_templates/1 call below
   # and add pages to the error directory:
   #
-  #   * lib/elixir_china_web/controllers/error/404.html.heex
-  #   * lib/elixir_china_web/controllers/error/500.html.heex
+  #   * lib/elixir_china_web/controllers/error_html/404.html.heex
+  #   * lib/elixir_china_web/controllers/error_html/500.html.heex
   #
-  # embed_templates "error/*"
 
-  # The default is to render a plain text page based on
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
-  def render(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+  embed_templates "error_html/*"
+
+  # render the fallback template
+  def render(template, assigns) do
+    status = assigns.status
+    message = Phoenix.Controller.status_message_from_template(template)
+    fallback(%{status: status, message: message})
   end
 end
